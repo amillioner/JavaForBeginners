48a3db0b0a78a13cab73eb1d942ceb9f7ebda6d0
amillioner <anwar.akhror@gmail.com>
2019-09-19 22:41:54 -0400

    Add changes for chapter 1.

---
+++ src/techno/study/ch1/Product.java
@@ -0,0 +1,24 @@
+package techno.study.ch1;
+// Ex. 2.6: Product.java
+// Calculate the product of three integers.
+import java.util.Scanner; // program uses Scanner
+
+public class Product {
+    public static void main(String[] args) {
+        // create Scanner to obtain input from command window
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: "); // prompt for input
+        int x = input.nextInt(); // read first integer
+
+        System.out.print("Enter second integer: "); // prompt for input
+        int y = input.nextInt(); // read second integer
+
+        System.out.print("Enter third integer: "); // prompt for input
+        int z = input.nextInt(); // read third integer
+
+        int result = x * y * z; // calculate product of numbers
+
+        System.out.printf("Product is %d%n", result);
+    } // end method main
+} // end class Product
---
+++ src/techno/study/ch1/Welcome4.java
@@ -0,0 +1,11 @@
+package techno.study.ch1;
+
+// Fig. 2.6: Welcome4.java
+// Displaying multiple lines with method System.out.printf.
+
+public class Welcome4 {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // end method main
+} // end class Welcome4
